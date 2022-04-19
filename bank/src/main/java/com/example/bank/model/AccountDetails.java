251d5a6d7d172c3150aeea983bfa7c066beafe70
vicky200620 <95837822+vicky200620@users.noreply.github.com>
2022-04-19 14:12:26 +0530

    Update AccountDetails.java

--- bank/src/main/java/com/example/bank/model/AccountDetails.java
+++ bank/src/main/java/com/example/bank/model/AccountDetails.java
@@ -4,20 +4,41 @@ import java.util.Date;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Positive;
+
+import org.springframework.format.annotation.DateTimeFormat;
 @Entity
 public class AccountDetails {
-   @Id
+    @Id
+    @Digits(message="AccountID should contain 10 digits.", fraction = 0, integer = 10)
 	private String accountID;
 	private String customerName;
 	private int pan;
+	@DateTimeFormat(pattern = "dd/MM/yyyy")
+    @Past(message = "Enter valid date.")
 	private Date DateOfBirth;
 	private String accountType;
 	private String accountState;
+    @NotNull
+    @Positive
 	private long accountBalance;
+    @Digits(message="Mobile number should contain 10 digits.", fraction = 0, integer = 10)
 	private String mobileNumber;
+	@NotBlank
+	@Email(message="Email should be valid")
 	private String email;
+	@Min(value = 1000, message = "Monthly average balance must be greater than 1000")
 	private long monthlyAverageBalance;
-	
+	public String getAccountID() {
+		return accountID;
+	}
 	public AccountDetails(String accountID, String customerName, int pan, Date dateOfBirth, String accountType,
 			String accountState, long accountBalance, String mobileNumber, String email, long monthlyAverageBalance) {
 		super();
@@ -31,10 +52,6 @@ public class AccountDetails {
 		this.mobileNumber = mobileNumber;
 		this.email = email;
 		this.monthlyAverageBalance = monthlyAverageBalance;
-	}
-	
-	public String getAccountID() {
-		return accountID;
 	}
 	public void setAccountID(String accountID) {
 		this.accountID = accountID;
